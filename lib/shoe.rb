957d434448cfa8e8af042143178e4067bc4f2b6c
init.defaultBranch <benlimpic@gmail.com>
2022-04-28 08:38:59 -0700

    commit tree 3d86e7cbb8bd8d3b426741f9d3177de7b756cd5a

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+# require "pry"
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        @size = "10,000+"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+# binding.pry
+# 0
